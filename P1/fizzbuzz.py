86d30c624037d5bd72082462368c733b48c7a5fc
Ricardo Hernandez <ricardo.ohg96@gmail.com>
2019-02-01 15:21:06 -0600

    Quitar parentesis

--- P1/fizzbuzz.py
+++ P1/fizzbuzz.py
@@ -11,4 +11,4 @@
 		
 
 # Fizz buzz en una sola linea
-print(["fizzbuzz" if a%3==0 and a%5==0 else ("buzz" if a%5==0 else ("fizz" if a%3==0 else a)) for a in range(1,31)])
+print(["fizzbuzz" if a%3==0 and a%5==0 else "buzz" if a%5==0 else "fizz" if a%3==0 else a for a in range(1,31)])
