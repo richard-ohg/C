c05e38d1dfa80296f43522975bce9ef6c58c6e1f
Ricardo Hernandez <ricardo.ohg96@gmail.com>
2019-02-01 00:24:53 -0600

    Fizzbuzz en una sola linea

--- P1/fizzbuzz.py
+++ P1/fizzbuzz.py
@@ -1,10 +1,14 @@
-for i in range(1,31):
-	if i%5 == 0 and i%3 == 0:
-		print("fizzbuzz")
-	elif i%3 == 0:
-		print("fizz")
-	elif i%5 == 0:
-		print("buzz")
-	else:
-		print(i)
+
+# for i in range(1,31):
+# 	if i%5 == 0 and i%3 == 0:
+# 		print("fizzbuzz")
+# 	elif i%3 == 0:
+# 		print("fizz")
+# 	elif i%5 == 0:
+# 		print("buzz")
+# 	else:
+# 		print(i)
 		
+
+# Fizz buzz en una sola linea
+print(["fizzbuzz" if a%3==0 and a%5==0 else ("buzz" if a%5==0 else ("fizz" if a%3==0 else a)) for a in range(1,31)])
