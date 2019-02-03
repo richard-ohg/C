8001b40d26ee39ce297ec3bed6443d3389421ccc
Ricardo Hernandez <ricardo.ohg96@gmail.com>
2019-02-03 03:57:46 -0600

    Cambie if elseif por un operador ternario

---
+++ P4/shrink_me.c
@@ -0,0 +1,13 @@
+//Practica 4
+//"Hackea" este programa
+
+#include<stdio.h>
+#include<limits.h>
+int main()
+{
+	unsigned int var1,var2;
+	printf("Ingresa dos enteros sin signo(maximo %u):",UINT_MAX);
+	scanf("%d",&var1);
+	scanf("%d",&var2);
+	printf("El mayor es %d\n",(var1 > var2) ? var1:var2);
+}
