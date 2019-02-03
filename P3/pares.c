e41b5a00cb3f0b0af1f407ff1dc2c6ab47e88679
Ricardo Hernandez <ricardo.ohg96@gmail.com>
2019-02-03 03:13:50 -0600

    Correcion de programa para utilizar linea de comandos

--- P3/pares.c
+++ P3/pares.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include <stdbool.h>
+#include <stdlib.h>
 
 int main(int argc, char const *argv[])
 {
-	for (int i = 0; i < 20; i++)
-		printf("%d %s\n",i, (i&1 == 1) ? "impar":"par");
-		// printf("%d\n", 11&1);
+	// for (int i = 0; i < 20; i++)
+	// 	printf("%d %s\n",i, (i&1 == 1) ? "impar":"par");
+	printf("El número: %d es %s\n",atoi(argv[1]), (atoi(argv[1])&1 == 1) ? "impar":"par" );
 	return 0;
 }
