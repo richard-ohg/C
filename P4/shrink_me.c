054b9f217e7771a04dbff0d0d20f23833d5cbcb3
Ricardo Hernandez <ricardo.ohg96@gmail.com>
2019-02-05 14:02:21 -0600

    Menos lineas

--- P4/shrink_me.c
+++ P4/shrink_me.c
@@ -6,8 +6,7 @@
 int main()
 {
 	unsigned int var1,var2;
-	printf("Ingresa dos enteros sin signo(maximo %u):",UINT_MAX);
-	scanf("%d",&var1);
-	scanf("%d",&var2);
+	printf("Ingresa dos enteros sin signo(maximo %u): ",UINT_MAX);
+	scanf("%d %d",&var1,&var2);
 	printf("El mayor es %d\n",(var1 > var2) ? var1:var2);
 }
