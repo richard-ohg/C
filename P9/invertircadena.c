992fd52d2bd875616bd025d542692f4f365e453f
Ricardo Hernandez <ricardo.ohg96@gmail.com>
2019-02-06 11:50:04 -0600

    Invertir cadena de otra forma

--- P9/invertircadena.c
+++ P9/invertircadena.c
@@ -6,9 +6,18 @@ int main(int argc, char const *argv[])
 	char *cadena = "ricardo";
 	char *aux = &cadena[strlen(cadena)-1];
 
+	// Recorrer con apuntadores de forma negativa, teniendo el indice 0 en la última posición de la cadena
 	for (int i = 0; i < strlen(cadena); ++i)
 	{
-		printf("%c\n",*(aux-i));
+		printf("%c ",*(aux-i));
+	}
+
+	printf("\n");
+
+	// Recorriendo con apuntadores de forma
+	for (int i = strlen(cadena)-1 ; i >= 0; --i)
+	{
+		printf("%c ",*(cadena+i));
 	}
 
 	return 0;
