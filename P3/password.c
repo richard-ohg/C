8419f6746552a2cbc09d6e876d0cc7b66595e228
Ricardo Hernandez <ricardo.ohg96@gmail.com>
2019-02-11 02:14:56 -0600

    Correcion de errores pequeños

--- P3/password.c
+++ P3/password.c
@@ -4,17 +4,16 @@
 
 int main(int argc, char const *argv[])
 {
-	char pass[50];
+	char pass_in[50];
 	for (int i = 0; i < 3; ++i)
 	{
 		printf("Ingresa tu password: ");
-		scanf("%s",pass);
-		if (strcmp(pass,"hola123,") == 0){
+		scanf("%s",pass_in);
+		// fgets(pass_in,50,stdin);
+		if (strcmp(pass_in,"hola123,") == 0){
 			printf("Contraseña correcta\n");
 			break;
 		}
-		else if (i==2)
-			exit(1);
 		else
 			printf("Contraseña incorrecta\n");	
 	}
